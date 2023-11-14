a8ef04b3d24f11ffe3c66423b72b671cebb10789
Bernardo-MG <programming@bernardomg.com>
2023-11-14 14:05:57 +0100

    Added tests for queries

--- src/test/java/com/bernardomg/security/authentication/user/test/util/model/Users.java
+++ src/test/java/com/bernardomg/security/authentication/user/test/util/model/Users.java
@@ -4,14 +4,19 @@ package com.bernardomg.security.authentication.user.test.util.model;
 import com.bernardomg.security.authentication.user.persistence.model.PersistentUser;
 
 public final class Users {
+    
+    public static String NAME = "Admin";
+    public static String USERNAME = "admin";
+    public static String EMAIL = "mail@somewhere.com";
+    public static String PASSWORD = "1234";
 
     public static final PersistentUser disabled() {
         return PersistentUser.builder()
             .id(1L)
-            .name("Name")
-            .username("username")
-            .email("mail@somewhere.com")
-            .password("1234")
+            .name(NAME)
+            .username(USERNAME)
+            .email(EMAIL)
+            .password(PASSWORD)
             .enabled(false)
             .expired(false)
             .passwordExpired(false)
@@ -22,10 +27,10 @@ public final class Users {
     public static final PersistentUser enabled() {
         return PersistentUser.builder()
             .id(1L)
-            .name("Name")
-            .username("username")
-            .email("mail@somewhere.com")
-            .password("1234")
+            .name(NAME)
+            .username(USERNAME)
+            .email(EMAIL)
+            .password(PASSWORD)
             .enabled(true)
             .expired(false)
             .passwordExpired(false)
@@ -36,10 +41,10 @@ public final class Users {
     public static final PersistentUser expired() {
         return PersistentUser.builder()
             .id(1L)
-            .name("Name")
-            .username("username")
-            .email("mail@somewhere.com")
-            .password("1234")
+            .name(NAME)
+            .username(USERNAME)
+            .email(EMAIL)
+            .password(PASSWORD)
             .enabled(true)
             .expired(true)
             .passwordExpired(false)
@@ -50,10 +55,10 @@ public final class Users {
     public static final PersistentUser locked() {
         return PersistentUser.builder()
             .id(1L)
-            .name("Name")
-            .username("username")
-            .email("mail@somewhere.com")
-            .password("1234")
+            .name(NAME)
+            .username(USERNAME)
+            .email(EMAIL)
+            .password(PASSWORD)
             .enabled(true)
             .expired(false)
             .passwordExpired(false)
@@ -64,10 +69,10 @@ public final class Users {
     public static final PersistentUser passwordExpired() {
         return PersistentUser.builder()
             .id(1L)
-            .name("Name")
-            .username("username")
-            .email("mail@somewhere.com")
-            .password("1234")
+            .name(NAME)
+            .username(USERNAME)
+            .email(EMAIL)
+            .password(PASSWORD)
             .enabled(true)
             .expired(false)
             .passwordExpired(true)
