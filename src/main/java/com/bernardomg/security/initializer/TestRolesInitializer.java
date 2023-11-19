d5bf64946e3d23cb3f43455c295c195b8d3e94d8
Bernardo-MG <programming@bernardomg.com>
2023-11-19 14:11:21 +0100

    Added development initializers

---
+++ src/main/java/com/bernardomg/security/initializer/TestRolesInitializer.java
@@ -0,0 +1,151 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2023 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.bernardomg.security.initializer;
+
+import java.util.Collection;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+import com.bernardomg.security.authorization.permission.constant.Actions;
+import com.bernardomg.security.authorization.permission.persistence.model.ResourcePermissionEntity;
+import com.bernardomg.security.authorization.permission.persistence.model.RolePermissionEntity;
+import com.bernardomg.security.authorization.permission.persistence.repository.ResourcePermissionRepository;
+import com.bernardomg.security.authorization.permission.persistence.repository.RolePermissionRepository;
+import com.bernardomg.security.authorization.role.persistence.model.RoleEntity;
+import com.bernardomg.security.authorization.role.persistence.repository.RoleRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Creates initial test roles on app start. These are meant to allow local development.
+ *
+ * @author Bernardo Mart&iacute;nez Garrido
+ *
+ */
+@Slf4j
+public final class TestRolesInitializer implements ApplicationRunner {
+
+    private final ResourcePermissionRepository permissionRepository;
+
+    private final RolePermissionRepository     rolePermissionRepository;
+
+    private final RoleRepository               roleRepository;
+
+    public TestRolesInitializer(final ResourcePermissionRepository permissionRepo, final RoleRepository roleRepo,
+            final RolePermissionRepository rolePermissionRepo) {
+        super();
+
+        permissionRepository = permissionRepo;
+        roleRepository = roleRepo;
+        rolePermissionRepository = rolePermissionRepo;
+    }
+
+    @Override
+    public final void run(final ApplicationArguments args) throws Exception {
+        final Collection<ResourcePermissionEntity> permissions;
+
+        log.debug("Initializing test roles");
+
+        permissions = permissionRepository.findAll();
+
+        runIfNotExists(() -> initializeAdminRole(permissions), "ADMIN");
+        runIfNotExists(() -> initializeReadRole(permissions), "READ");
+
+        log.debug("Initialized test roles");
+    }
+
+    private final RoleEntity getReadRole() {
+        return RoleEntity.builder()
+            .name("READ")
+            .build();
+    }
+
+    private final RoleEntity getRootRole() {
+        return RoleEntity.builder()
+            .name("ADMIN")
+            .build();
+    }
+
+    private final void initializeAdminRole(final Collection<ResourcePermissionEntity> permissions) {
+        final RoleEntity     rootRole;
+        final RoleEntity     savedRootRole;
+        RolePermissionEntity rolePermission;
+
+        // Add read user
+        rootRole = getRootRole();
+        savedRootRole = roleRepository.save(rootRole);
+
+        for (final ResourcePermissionEntity perm : permissions) {
+            rolePermission = RolePermissionEntity.builder()
+                .roleId(savedRootRole.getId())
+                .permissionId(perm.getId())
+                .granted(true)
+                .build();
+            rolePermissionRepository.save(rolePermission);
+        }
+    }
+
+    private final void initializeReadRole(final Collection<ResourcePermissionEntity> permissions) {
+        final RoleEntity readRole;
+        final RoleEntity savedReadRole;
+
+        // Add read user
+        readRole = getReadRole();
+        savedReadRole = roleRepository.save(readRole);
+
+        setPermissions(savedReadRole, permissions, Actions.READ);
+        setPermissions(savedReadRole, permissions, Actions.VIEW);
+    }
+
+    private final void runIfNotExists(final Runnable runnable, final String name) {
+        if (!roleRepository.existsByName(name)) {
+            runnable.run();
+            log.debug("Initialized {} role", name);
+        } else {
+            log.debug("Role {} already exists. Skipped initialization.", name);
+        }
+    }
+
+    private final void setPermissions(final RoleEntity role, final Collection<ResourcePermissionEntity> permissions,
+            final String actionName) {
+        RolePermissionEntity                       rolePermission;
+        final Collection<ResourcePermissionEntity> validPermissions;
+
+        validPermissions = permissions.stream()
+            .filter(p -> p.getAction()
+                .equals(actionName))
+            .toList();
+        for (final ResourcePermissionEntity permission : validPermissions) {
+            rolePermission = RolePermissionEntity.builder()
+                .roleId(role.getId())
+                .permissionId(permission.getId())
+                .granted(true)
+                .build();
+            rolePermissionRepository.save(rolePermission);
+        }
+    }
+
+}
