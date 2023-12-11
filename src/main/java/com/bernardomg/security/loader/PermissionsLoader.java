abd3804fe29fc8515b2c8676e0d0d420705340ee
Bernardo-MG <programming@bernardomg.com>
2023-12-11 22:25:34 +0100

    Added javadocs

--- src/main/java/com/bernardomg/security/loader/PermissionsLoader.java
+++ src/main/java/com/bernardomg/security/loader/PermissionsLoader.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2023 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 package com.bernardomg.security.loader;
 
@@ -13,6 +36,11 @@ import com.bernardomg.security.authorization.permission.persistence.repository.A
 import com.bernardomg.security.authorization.permission.persistence.repository.ResourcePermissionRepository;
 import com.bernardomg.security.authorization.permission.persistence.repository.ResourceRepository;
 
+/**
+ * Loads the permissions configuration for the application. These are loaded from a list of {@link PermissionRegister}.
+ * <p>
+ * The {@link #load()} method takes care of persisting all the data.
+ */
 public final class PermissionsLoader {
 
     private final ActionRepository               actionRepository;
@@ -33,6 +61,9 @@ public final class PermissionsLoader {
         permissions = Objects.requireNonNull(perms);
     }
 
+    /**
+     * Persists the permissions.
+     */
     public final void load() {
         // Load actions
         permissions.stream()
