d069b1c822133d7c26c251262be15cabec8de215
Bernardo <programming@bernardomg.com>
2023-12-24 15:04:49 +0100

    Fixed permissions query error

--- src/main/java/com/bernardomg/security/authorization/permission/persistence/repository/ResourcePermissionRepository.java
+++ src/main/java/com/bernardomg/security/authorization/permission/persistence/repository/ResourcePermissionRepository.java
@@ -52,7 +52,7 @@ public interface ResourcePermissionRepository extends JpaRepository<ResourcePerm
      *            pagination to apply
      * @return a page with the permissions
      */
-    @Query("SELECT p FROM ResourcePermission p LEFT JOIN RolePermission rp ON p.name = rp.permission WHERE rp.roleId = :roleId AND (rp.permission IS NULL OR rp.granted = false)")
+    @Query("SELECT p FROM ResourcePermission p LEFT JOIN RolePermission rp ON p.name = rp.permission AND rp.roleId = :roleId WHERE rp.permission IS NULL OR rp.granted = false")
     public Page<ResourcePermissionEntity> findAllAvailableToRole(@Param("roleId") final Long roleId,
             final Pageable page);
 
@@ -65,7 +65,7 @@ public interface ResourcePermissionRepository extends JpaRepository<ResourcePerm
      *            pagination to apply
      * @return a page with the permissions
      */
-    @Query("SELECT p FROM ResourcePermission p INNER JOIN RolePermission rp ON p.name = rp.permission WHERE rp.roleId = :roleId AND rp.granted = true")
+    @Query("SELECT p FROM ResourcePermission p INNER JOIN RolePermission rp ON p.name = rp.permission AND rp.roleId = :roleId WHERE rp.granted = true")
     public Page<ResourcePermissionEntity> findAllForRole(@Param("roleId") final Long roleId, final Pageable page);
 
     /**
--- src/test/java/com/bernardomg/security/authorization/permission/test/integration/service/ITRolePermissionServiceGetAvailablePermissions.java
+++ src/test/java/com/bernardomg/security/authorization/permission/test/integration/service/ITRolePermissionServiceGetAvailablePermissions.java
@@ -9,6 +9,8 @@ import org.springframework.data.domain.Pageable;
 
 import com.bernardomg.security.authorization.permission.model.ResourcePermission;
 import com.bernardomg.security.authorization.permission.service.RolePermissionService;
+import com.bernardomg.security.authorization.permission.test.config.AlternativeRoleWithCrudPermissions;
+import com.bernardomg.security.authorization.permission.test.config.AlternativeRoleWithCrudPermissionsNotGranted;
 import com.bernardomg.security.authorization.permission.test.config.CrudPermissions;
 import com.bernardomg.security.authorization.permission.test.config.RoleWithCrudPermissions;
 import com.bernardomg.security.authorization.permission.test.config.RoleWithCrudPermissionsNotGranted;
@@ -62,10 +64,46 @@ class ITRolePermissionServiceGetAvailablePermissions {
     }
 
     @Test
+    @DisplayName("When all the permission have been assigned, and there is another role with no permissions, nothing is returned")
+    @RoleWithCrudPermissions
+    @AlternativeRoleWithCrudPermissionsNotGranted
+    void testGetAvailablePermissions_AllAssigned_AlternativeRole() {
+        final Iterable<ResourcePermission> permissions;
+        final Pageable                     pageable;
+
+        pageable = Pageable.unpaged();
+
+        permissions = service.getAvailablePermissions(Roles.NAME, pageable);
+
+        Assertions.assertThat(permissions)
+            .as("permissions")
+            .isEmpty();
+    }
+
+    @Test
     @DisplayName("When the role has no permissions all the permissions are returned")
     @CrudPermissions
     @SingleRole
     void testGetAvailablePermissions_NoPermissions() {
+        final Iterable<ResourcePermission> permissions;
+        final Pageable                     pageable;
+
+        pageable = Pageable.unpaged();
+
+        permissions = service.getAvailablePermissions(Roles.NAME, pageable);
+
+        Assertions.assertThat(permissions)
+            .as("permissions")
+            .containsOnly(ResourcePermissions.create(), ResourcePermissions.read(), ResourcePermissions.update(),
+                ResourcePermissions.delete());
+    }
+
+    @Test
+    @DisplayName("When the role has no permissions, and there is another role with all permissions, all the permissions are returned")
+    @CrudPermissions
+    @SingleRole
+    @AlternativeRoleWithCrudPermissions
+    void testGetAvailablePermissions_NoPermissions_AlternativeRole() {
         final Iterable<ResourcePermission> permissions;
         final Pageable                     pageable;
 
