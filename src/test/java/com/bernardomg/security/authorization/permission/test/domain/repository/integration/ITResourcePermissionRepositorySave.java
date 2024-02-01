2f3c87b0ecf5c3e66191c858d39ca89f9a77ea32
Bernardo-MG <programming@bernardomg.com>
2024-02-01 23:44:34 +0100

    Added tests

---
+++ src/test/java/com/bernardomg/security/authorization/permission/test/domain/repository/integration/ITResourcePermissionRepositorySave.java
@@ -0,0 +1,73 @@
+
+package com.bernardomg.security.authorization.permission.test.domain.repository.integration;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bernardomg.security.authorization.permission.adapter.inbound.jpa.model.ResourcePermissionEntity;
+import com.bernardomg.security.authorization.permission.adapter.inbound.jpa.repository.ResourcePermissionSpringRepository;
+import com.bernardomg.security.authorization.permission.domain.model.ResourcePermission;
+import com.bernardomg.security.authorization.permission.domain.repository.ResourcePermissionRepository;
+import com.bernardomg.security.authorization.permission.test.config.annotation.ResourceAndActions;
+import com.bernardomg.security.authorization.permission.test.config.factory.ResourcePermissionEntities;
+import com.bernardomg.security.authorization.permission.test.config.factory.ResourcePermissions;
+import com.bernardomg.test.config.annotation.IntegrationTest;
+
+@IntegrationTest
+@DisplayName("ResourcePermissionRepository - remove permission")
+class ITResourcePermissionRepositorySave {
+
+    @Autowired
+    private ResourcePermissionRepository       repository;
+
+    @Autowired
+    private ResourcePermissionSpringRepository resourcePermissionSpringRepository;
+
+    public ITResourcePermissionRepositorySave() {
+        super();
+    }
+
+    @Test
+    @DisplayName("Persists the data")
+    @ResourceAndActions
+    void testRemovePermission_Persisted() {
+        final Iterable<ResourcePermissionEntity> permissions;
+        final ResourcePermission                 permission;
+
+        // GIVEN
+        permission = ResourcePermissions.create();
+
+        // WHEN
+        repository.save(permission);
+
+        // THEN
+        permissions = resourcePermissionSpringRepository.findAll();
+
+        Assertions.assertThat(permissions)
+            .as("permissions")
+            .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
+            .containsOnly(ResourcePermissionEntities.create());
+    }
+
+    @Test
+    @DisplayName("Returns the persisted data")
+    @ResourceAndActions
+    void testRemovePermission_Returned() {
+        final ResourcePermission created;
+        final ResourcePermission permission;
+
+        // GIVEN
+        permission = ResourcePermissions.create();
+
+        // WHEN
+        created = repository.save(permission);
+
+        // THEN
+        Assertions.assertThat(created)
+            .as("permissions")
+            .isEqualTo(ResourcePermissions.create());
+    }
+
+}
