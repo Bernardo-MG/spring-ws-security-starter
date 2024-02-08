9adbbe0bf4dfb09c77e215d0594320fba644a359
Bernardo-MG <programming@bernardomg.com>
2024-02-08 22:21:57 +0100

    Added javadocs

--- src/main/java/com/bernardomg/security/authentication/user/domain/repository/UserRepository.java
+++ src/main/java/com/bernardomg/security/authentication/user/domain/repository/UserRepository.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2023 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 package com.bernardomg.security.authentication.user.domain.repository;
 
@@ -9,13 +32,18 @@ import com.bernardomg.security.authentication.user.domain.model.User;
 import com.bernardomg.security.authentication.user.domain.model.UserChange;
 import com.bernardomg.security.authentication.user.domain.model.UserQuery;
 
+/**
+ * User repository.
+ *
+ * @author Bernardo Mart&iacute;nez Garrido
+ */
 public interface UserRepository {
 
     /**
      * Deletes the user with the received username.
      *
      * @param username
-     *            username to delete
+     *            user to delete
      */
     public void delete(final String username);
 
@@ -24,14 +52,35 @@ public interface UserRepository {
      *
      * @param username
      *            username to search for
-     * @return {@code true} if a user exists, {@code false} otherwise
+     * @return {@code true} if the user exists, {@code false} otherwise
      */
     public boolean exists(final String username);
 
+    /**
+     * Returns whether an user with the given email exists.
+     *
+     * @param email
+     *            email to search for
+     * @return {@code true} if the user exists, {@code false} otherwise
+     */
     public boolean existsByEmail(final String email);
 
+    /**
+     * Returns whether an user, other than the one received, has the email.
+     * 
+     * @param username user to ignore
+     * @param email email to search for
+     * @return {@code true} if another user has the email, {@code false} otherwise
+     */
     public boolean existsEmailForAnotherUser(final String username, final String email);
 
+    /**
+     * Returns all the user for the received query.
+     * 
+     * @param query query to filter the users
+     * @param page pagination to apply
+     * @return all the user for the received query
+     */
     public Iterable<User> findAll(final UserQuery query, final Pageable page);
 
     /**
@@ -43,8 +92,20 @@ public interface UserRepository {
      */
     public Optional<User> findOne(final String username);
 
+    /**
+     * Returns the user for the received email.
+     *
+     * @param email
+     *            email to search for
+     * @return the user for the received email
+     */
     public Optional<User> findOneByEmail(final String email);
 
+    /**
+     * Returns the password for the user.
+     * @param username user to search for the password
+     * @return the user password
+     */
     public Optional<String> findPassword(final String username);
 
     public User save(final String username, final UserChange user);
