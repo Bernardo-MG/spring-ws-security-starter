c24f85e19155f7035009ee5a6a07752c67b996d4
Bernardo-MG <programming@bernardomg.com>
2024-04-07 12:24:18 +0200

    Allows extending account output

--- src/test/java/com/bernardomg/security/account/test/config/factory/Accounts.java
+++ src/test/java/com/bernardomg/security/account/test/config/factory/Accounts.java
@@ -2,12 +2,13 @@
 package com.bernardomg.security.account.test.config.factory;
 
 import com.bernardomg.security.account.domain.model.Account;
+import com.bernardomg.security.account.domain.model.BasicAccount;
 import com.bernardomg.security.authentication.user.test.config.factory.UserConstants;
 
 public final class Accounts {
 
     public static final Account nameChange() {
-        return Account.builder()
+        return BasicAccount.builder()
             .withUsername(UserConstants.USERNAME)
             .withName(UserConstants.ALTERNATIVE_NAME)
             .withEmail(UserConstants.EMAIL)
@@ -15,7 +16,7 @@ public final class Accounts {
     }
 
     public static final Account valid() {
-        return Account.builder()
+        return BasicAccount.builder()
             .withUsername(UserConstants.USERNAME)
             .withName(UserConstants.NAME)
             .withEmail(UserConstants.EMAIL)
