533e521fb94ec3c8b1800ef6059527ef659ac452
Bernardo-MG <programming@bernardomg.com>
2024-04-28 01:39:21 +0200

    Cleaned up test

--- src/test/java/com/bernardomg/security/authentication/user/test/usecase/service/unit/TestUserActivationServiceActivateUser.java
+++ src/test/java/com/bernardomg/security/authentication/user/test/usecase/service/unit/TestUserActivationServiceActivateUser.java
@@ -6,6 +6,9 @@ import static org.mockito.Mockito.verify;
 
 import java.util.Optional;
 
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,8 +16,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import com.bernardomg.security.authentication.jwt.token.test.config.Tokens;
+import com.bernardomg.security.authentication.user.domain.exception.EnabledUserException;
+import com.bernardomg.security.authentication.user.domain.exception.ExpiredUserException;
+import com.bernardomg.security.authentication.user.domain.exception.LockedUserException;
+import com.bernardomg.security.authentication.user.domain.exception.MissingUserException;
 import com.bernardomg.security.authentication.user.domain.repository.UserRepository;
 import com.bernardomg.security.authentication.user.test.config.factory.UserConstants;
 import com.bernardomg.security.authentication.user.test.config.factory.Users;
@@ -45,6 +53,11 @@ class TestUserActivationServiceActivateUser {
         super();
     }
 
+    @BeforeEach
+    public void initializeToken() {
+        given(tokenStore.getUsername(Tokens.TOKEN)).willReturn(UserConstants.USERNAME);
+    }
+
     @Test
     @DisplayName("Activating a new user consumes the token")
     void testActivateUser_ConsumesToken() {
@@ -64,13 +77,52 @@ class TestUserActivationServiceActivateUser {
     void testActivateUser_Disabled() {
         // GIVEN
         given(repository.findOne(UserConstants.USERNAME)).willReturn(Optional.of(Users.disabled()));
-        given(tokenStore.getUsername(Tokens.TOKEN)).willReturn(UserConstants.USERNAME);
 
         // WHEN
         service.activateUser(Tokens.TOKEN, UserConstants.PASSWORD);
 
         // THEN
         verify(repository).save(Users.enabled(), UserConstants.PASSWORD);
+    }
+
+    @Test
+    @WithMockUser(username = "username")
+    @DisplayName("Activating an enabled user gives a failure")
+    void testActivateUser_Enabled_Exception() {
+        final ThrowingCallable executable;
+        final Exception        exception;
+
+        // GIVEN
+        given(repository.findOne(UserConstants.USERNAME)).willReturn(Optional.of(Users.enabled()));
+
+        // WHEN
+        executable = () -> service.activateUser(Tokens.TOKEN, UserConstants.PASSWORD);
+
+        // THEN
+        exception = Assertions.catchThrowableOfType(executable, EnabledUserException.class);
+
+        Assertions.assertThat(exception.getMessage())
+            .isEqualTo("User " + UserConstants.USERNAME + " is enabled");
+    }
+
+    @Test
+    @WithMockUser(username = "username")
+    @DisplayName("Activating a expired user gives a failure")
+    void testActivateUser_Expired_Exception() {
+        final ThrowingCallable executable;
+        final Exception        exception;
+
+        // GIVEN
+        given(repository.findOne(UserConstants.USERNAME)).willReturn(Optional.of(Users.expired()));
+
+        // WHEN
+        executable = () -> service.activateUser(Tokens.TOKEN, UserConstants.PASSWORD);
+
+        // THEN
+        exception = Assertions.catchThrowableOfType(executable, ExpiredUserException.class);
+
+        Assertions.assertThat(exception.getMessage())
+            .isEqualTo("User " + UserConstants.USERNAME + " is expired");
     }
 
     @Test
@@ -78,7 +130,6 @@ class TestUserActivationServiceActivateUser {
     void testActivateUser_KeepsRoles() {
         // GIVEN
         given(repository.findOne(UserConstants.USERNAME)).willReturn(Optional.of(Users.newlyCreatedWithRole()));
-        given(tokenStore.getUsername(Tokens.TOKEN)).willReturn(UserConstants.USERNAME);
 
         // WHEN
         service.activateUser(Tokens.TOKEN, UserConstants.PASSWORD);
@@ -88,11 +139,30 @@ class TestUserActivationServiceActivateUser {
     }
 
     @Test
+    @WithMockUser(username = "username")
+    @DisplayName("Activating a locked user gives a failure")
+    void testActivateUser_Locked_Exception() {
+        final ThrowingCallable executable;
+        final Exception        exception;
+
+        // GIVEN
+        given(repository.findOne(UserConstants.USERNAME)).willReturn(Optional.of(Users.locked()));
+
+        // WHEN
+        executable = () -> service.activateUser(Tokens.TOKEN, UserConstants.PASSWORD);
+
+        // THEN
+        exception = Assertions.catchThrowableOfType(executable, LockedUserException.class);
+
+        Assertions.assertThat(exception.getMessage())
+            .isEqualTo("User " + UserConstants.USERNAME + " is locked");
+    }
+
+    @Test
     @DisplayName("Activating a new user saves it as enabled")
     void testActivateUser_NewlyCreated() {
         // GIVEN
         given(repository.findOne(UserConstants.USERNAME)).willReturn(Optional.of(Users.newlyCreated()));
-        given(tokenStore.getUsername(Tokens.TOKEN)).willReturn(UserConstants.USERNAME);
 
         // WHEN
         service.activateUser(Tokens.TOKEN, UserConstants.PASSWORD);
@@ -102,11 +172,30 @@ class TestUserActivationServiceActivateUser {
     }
 
     @Test
+    @WithMockUser(username = "username")
+    @DisplayName("Activating a not existing user gives a failure")
+    void testActivateUser_NotExistingUser_Exception() {
+        final ThrowingCallable executable;
+        final Exception        exception;
+
+        // GIVEN
+        given(repository.findOne(UserConstants.USERNAME)).willReturn(Optional.empty());
+
+        // WHEN
+        executable = () -> service.activateUser(Tokens.TOKEN, UserConstants.PASSWORD);
+
+        // THEN
+        exception = Assertions.catchThrowableOfType(executable, MissingUserException.class);
+
+        Assertions.assertThat(exception.getMessage())
+            .isEqualTo("Missing id username for user");
+    }
+
+    @Test
     @DisplayName("Activating a user with password expired saves it as enabled")
     void testActivateUser_PasswordExpired() {
         // GIVEN
         given(repository.findOne(UserConstants.USERNAME)).willReturn(Optional.of(Users.passwordExpiredAndDisabled()));
-        given(tokenStore.getUsername(Tokens.TOKEN)).willReturn(UserConstants.USERNAME);
 
         // WHEN
         service.activateUser(Tokens.TOKEN, UserConstants.PASSWORD);
