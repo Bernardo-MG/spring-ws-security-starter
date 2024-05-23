0491dcb0db93a6a49656b55b3e6321c9850e7037
Bernardo <programming@bernardomg.com>
2024-05-23 17:18:12 +0200

    Corrected decoding tests

---
+++ src/test/java/com/bernardomg/security/authentication/jwt/token/test/config/JwtTokens.java
@@ -0,0 +1,14 @@
+
+package com.bernardomg.security.authentication.jwt.token.test.config;
+
+public final class JwtTokens {
+
+    public static final String TOKEN_WITH_SUBJECT         = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdWJqZWN0IiwicGVybWlzc2lvbnMiOnt9fQ.JLpqveWAso_ueYhaBBM0xBXNmzx980dTQlU2ZNIy_XQAnR_h5OMfhsdcNzYZ4E7xMj0nb-nsLPLzgKuk98E1qg";
+
+    public static final String TOKEN_WITH_SUBJECT_EXPIRED = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdWJqZWN0IiwicGVybWlzc2lvbnMiOnt9LCJleHAiOjE3MTY0NzczMDJ9.8iy9ccUVW3bh4OU9dtPG5jjtF5vmov5zD3sJqJGfAPAhcKoCzfVbl-SqbmnDRxTf975dRSJqey3T02ua2YzlYQ";
+
+    private JwtTokens() {
+        super();
+    }
+
+}
--- src/test/java/com/bernardomg/security/authentication/jwt/token/test/unit/TestJjwtTokenDecoderDecode.java
+++ src/test/java/com/bernardomg/security/authentication/jwt/token/test/unit/TestJjwtTokenDecoderDecode.java
@@ -6,34 +6,26 @@ import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import com.bernardomg.security.authentication.jwt.domain.JwtTokenData;
-import com.bernardomg.security.authentication.jwt.token.test.config.JwtTokenDatas;
+import com.bernardomg.security.authentication.jwt.token.test.config.JwtTokens;
 import com.bernardomg.security.authentication.jwt.token.test.config.Tokens;
 import com.bernardomg.security.authentication.jwt.usecase.encoding.JjwtTokenDecoder;
-import com.bernardomg.security.authentication.jwt.usecase.encoding.JjwtTokenEncoder;
 import com.bernardomg.security.authentication.jwt.usecase.encoding.TokenDecoder;
-import com.bernardomg.security.authentication.jwt.usecase.encoding.TokenEncoder;
 
 import io.jsonwebtoken.ExpiredJwtException;
 
-@DisplayName("JjwtTokenDecoder - get subject")
-class TestJjwtTokenDecoderGetSubject {
+@DisplayName("JjwtTokenDecoder - decode")
+class TestJjwtTokenDecoderDecode {
 
     private final TokenDecoder decoder = new JjwtTokenDecoder(Tokens.KEY);
 
-    private final TokenEncoder encoder = new JjwtTokenEncoder(Tokens.KEY);
-
     @Test
     @DisplayName("Recovers the subject from a token")
-    void testGetSubject_fromGeneratedToken() {
-        final String       token;
-        final String       subject;
-        final JwtTokenData data;
+    void testDecode_fromGeneratedToken() {
+        final String token;
+        final String subject;
 
         // GIVEN
-        data = JwtTokenDatas.withSubject();
-
-        token = encoder.encode(data);
+        token = JwtTokens.TOKEN_WITH_SUBJECT;
 
         // WHEN
         subject = decoder.decode(token)
@@ -47,20 +39,17 @@ class TestJjwtTokenDecoderGetSubject {
 
     @Test
     @DisplayName("Recovering the subject from an expired token generates an exception")
-    void testGetSubject_fromGeneratedToken_expired() {
+    void testDecode_fromGeneratedToken_expired() {
         final String           token;
         final ThrowingCallable executable;
-        final JwtTokenData     data;
 
         // GIVEN
-        data = JwtTokenDatas.withSubjectExpired();
+        token = JwtTokens.TOKEN_WITH_SUBJECT_EXPIRED;
 
         // WHEN
-        token = encoder.encode(data);
-
-        // THEN
         executable = () -> decoder.decode(token);
 
+        // THEN
         Assertions.assertThatThrownBy(executable)
             .isInstanceOf(ExpiredJwtException.class);
     }
