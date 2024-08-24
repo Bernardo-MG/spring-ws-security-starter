ba0c806667c52dc7c997576178cd495cb4253b9b
Bernardo-MG <programming@bernardomg.com>
2024-08-24 23:56:41 +0200

    Correcte white list filter

--- src/main/java/com/bernardomg/security/web/whitelist/WhitelistRoute.java
+++ src/main/java/com/bernardomg/security/web/whitelist/WhitelistRoute.java
@@ -43,17 +43,11 @@ import lombok.Value;
 @EqualsAndHashCode
 public final class WhitelistRoute {
 
-    public static WhitelistRoute of(final String route) {
-        return WhitelistRoute.builder()
-            .withRoute(route)
-            .withMethods(List.of())
-            .build();
-    }
-
     public static WhitelistRoute of(final String route, final HttpMethod... methods) {
         for (final HttpMethod method : methods) {
             Objects.requireNonNull(method);
         }
+
         return WhitelistRoute.builder()
             .withRoute(route)
             .withMethods(Arrays.asList(methods))
