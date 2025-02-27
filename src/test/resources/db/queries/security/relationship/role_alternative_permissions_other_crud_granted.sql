615d9f2553013d83d5e8a6a978b2df9ba5a687e0
Bernardo <programming@bernardomg.com>
2025-02-27 23:23:00 +0100

    Added tests

---
+++ src/test/resources/db/queries/security/relationship/role_alternative_permissions_other_crud_granted.sql
@@ -0,0 +1,5 @@
+INSERT INTO security.role_permissions (role_id, permission, granted) VALUES
+   (1, 'CREATE:DATA2', true),
+   (1, 'READ:DATA2', true),
+   (1, 'UPDATE:DATA2', true),
+   (1, 'DELETE:DATA2', true);
