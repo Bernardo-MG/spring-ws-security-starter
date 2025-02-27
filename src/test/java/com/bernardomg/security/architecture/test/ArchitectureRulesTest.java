15b093d9934f614e2d47d44b7c6132ee129b7526
Bernardo <programming@bernardomg.com>
2025-02-27 23:43:16 +0100

    Updated architecture tests

--- src/test/java/com/bernardomg/security/architecture/test/ArchitectureRulesTest.java
+++ src/test/java/com/bernardomg/security/architecture/test/ArchitectureRulesTest.java
@@ -1,17 +1,18 @@
 
 package com.bernardomg.security.architecture.test;
 
+import com.bernardomg.framework.testing.architecture.rule.CodingRules;
+import com.bernardomg.framework.testing.architecture.rule.DependencyRules;
+import com.bernardomg.framework.testing.architecture.rule.JpaEntityRules;
+import com.bernardomg.framework.testing.architecture.rule.RepositoryRules;
+import com.bernardomg.framework.testing.architecture.rule.ServiceRules;
+import com.bernardomg.framework.testing.architecture.rule.TransactionalRules;
+import com.bernardomg.framework.testing.architecture.rule.ValidationRules;
+import com.bernardomg.framework.testing.architecture.rule.springframework.SpringControllerRules;
+import com.bernardomg.framework.testing.architecture.rule.springframework.SpringRules;
 import com.bernardomg.security.architecture.rule.CacheRules;
-import com.bernardomg.security.architecture.rule.CodingRules;
-import com.bernardomg.security.architecture.rule.ConfigurationRules;
-import com.bernardomg.security.architecture.rule.ControllerRules;
-import com.bernardomg.security.architecture.rule.ImportRules;
-import com.bernardomg.security.architecture.rule.JpaEntityRules;
-import com.bernardomg.security.architecture.rule.RepositoryRules;
+import com.bernardomg.security.architecture.rule.LayerDependencyRules;
 import com.bernardomg.security.architecture.rule.SecurityRules;
-import com.bernardomg.security.architecture.rule.ServiceRules;
-import com.bernardomg.security.architecture.rule.TransactionalRules;
-import com.bernardomg.security.architecture.rule.ValidationRules;
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
@@ -21,36 +22,39 @@ import com.tngtech.archunit.junit.ArchTests;
 public class ArchitectureRulesTest {
 
     @ArchTest
-    static final ArchTests cacheRules         = ArchTests.in(CacheRules.class);
+    static final ArchTests cacheRules           = ArchTests.in(CacheRules.class);
 
     @ArchTest
-    static final ArchTests codingRules        = ArchTests.in(CodingRules.class);
+    static final ArchTests codingRules          = ArchTests.in(CodingRules.class);
 
     @ArchTest
-    static final ArchTests configurationRules = ArchTests.in(ConfigurationRules.class);
+    static final ArchTests configurationRules   = ArchTests.in(SpringRules.class);
 
     @ArchTest
-    static final ArchTests controllerRules    = ArchTests.in(ControllerRules.class);
+    static final ArchTests controllerRules      = ArchTests.in(SpringControllerRules.class);
 
     @ArchTest
-    static final ArchTests importRules        = ArchTests.in(ImportRules.class);
+    static final ArchTests dependencyRules      = ArchTests.in(DependencyRules.class);
 
     @ArchTest
-    static final ArchTests jpaEntityRules     = ArchTests.in(JpaEntityRules.class);
+    static final ArchTests jpaEntityRules       = ArchTests.in(JpaEntityRules.class);
 
     @ArchTest
-    static final ArchTests repositoryRules    = ArchTests.in(RepositoryRules.class);
+    static final ArchTests layerDependencyRules = ArchTests.in(LayerDependencyRules.class);
 
     @ArchTest
-    static final ArchTests securityRules      = ArchTests.in(SecurityRules.class);
+    static final ArchTests repositoryRules      = ArchTests.in(RepositoryRules.class);
 
     @ArchTest
-    static final ArchTests serviceRules       = ArchTests.in(ServiceRules.class);
+    static final ArchTests securityRules        = ArchTests.in(SecurityRules.class);
 
     @ArchTest
-    static final ArchTests transactionalRules = ArchTests.in(TransactionalRules.class);
+    static final ArchTests serviceRules         = ArchTests.in(ServiceRules.class);
 
     @ArchTest
-    static final ArchTests validationRules    = ArchTests.in(ValidationRules.class);
+    static final ArchTests transactionalRules   = ArchTests.in(TransactionalRules.class);
+
+    @ArchTest
+    static final ArchTests validationRules      = ArchTests.in(ValidationRules.class);
 
 }
