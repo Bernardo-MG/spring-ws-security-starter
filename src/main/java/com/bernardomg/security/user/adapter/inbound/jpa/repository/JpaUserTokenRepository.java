f061cbf1c2f8bdbb0fb09ba431860bbbda726d9a
Bernardo-MG <programming@bernardomg.com>
2025-09-18 00:03:12 +0200

    Corrected pagination

--- src/main/java/com/bernardomg/security/user/adapter/inbound/jpa/repository/JpaUserTokenRepository.java
+++ src/main/java/com/bernardomg/security/user/adapter/inbound/jpa/repository/JpaUserTokenRepository.java
@@ -98,8 +98,7 @@ public final class JpaUserTokenRepository implements UserTokenRepository {
         page = userDataTokenSpringRepository.findAll(pageable)
             .map(this::toDomain);
 
-        return new Page<>(page.getContent(), page.getSize(), page.getNumber(), page.getTotalElements(),
-            page.getTotalPages(), page.getNumberOfElements(), page.isFirst(), page.isLast(), sorting);
+        return SpringPagination.toPage(page);
     }
 
     @Override
