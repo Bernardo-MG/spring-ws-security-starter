5bf9c787b8c36aefeb7f9eb16609ae729cd7c978
Bernardo-MG <programming@bernardomg.com>
2025-09-24 00:04:44 +0200

    Fixed page mapping

--- src/main/java/com/bernardomg/security/login/adapter/outbound/rest/model/LoginRegisterDtoMapper.java
+++ src/main/java/com/bernardomg/security/login/adapter/outbound/rest/model/LoginRegisterDtoMapper.java
@@ -1,29 +1,57 @@
 
 package com.bernardomg.security.login.adapter.outbound.rest.model;
 
-import java.util.List;
-
 import com.bernardomg.data.domain.Page;
+import com.bernardomg.data.domain.Sorting.Direction;
+import com.bernardomg.data.domain.Sorting.Property;
 import com.bernardomg.security.login.domain.model.LoginRegister;
 import com.bernardomg.ucronia.openapi.model.LoginRegisterDto;
 import com.bernardomg.ucronia.openapi.model.LoginRegisterPageResponseDto;
+import com.bernardomg.ucronia.openapi.model.PropertyDto;
+import com.bernardomg.ucronia.openapi.model.PropertyDto.DirectionEnum;
+import com.bernardomg.ucronia.openapi.model.SortingDto;
 
 public final class LoginRegisterDtoMapper {
 
-    public static final LoginRegisterPageResponseDto toResponseDto(final Page<LoginRegister> loginRegisters) {
-        List<LoginRegisterDto> loginRegisterDtos;
+    public static final LoginRegisterPageResponseDto toResponseDto(final Page<LoginRegister> page) {
+        final SortingDto sortingResponse;
 
-        loginRegisterDtos = loginRegisters.content()
+        sortingResponse = new SortingDto().properties(page.sort()
+            .properties()
             .stream()
             .map(LoginRegisterDtoMapper::toDto)
-            .toList();
-        return new LoginRegisterPageResponseDto().content(loginRegisterDtos);
+            .toList());
+
+        return new LoginRegisterPageResponseDto().content(page.content()
+            .stream()
+            .map(LoginRegisterDtoMapper::toDto)
+            .toList())
+            .size(page.size())
+            .page(page.page())
+            .totalElements(page.totalElements())
+            .totalPages(page.totalPages())
+            .elementsInPage(page.elementsInPage())
+            .first(page.first())
+            .last(page.last())
+            .sort(sortingResponse);
     }
 
     private static final LoginRegisterDto toDto(final LoginRegister loginRegister) {
         return new LoginRegisterDto().username(loginRegister.username())
             .loggedIn(loginRegister.loggedIn())
             .date(loginRegister.date());
+    }
+
+    private static final PropertyDto toDto(final Property property) {
+        final DirectionEnum direction;
+
+        if (property.direction() == Direction.ASC) {
+            direction = DirectionEnum.ASC;
+        } else {
+            direction = DirectionEnum.DESC;
+        }
+        return new PropertyDto().name(property.name())
+            .direction(direction);
     }
 
     private LoginRegisterDtoMapper() {
