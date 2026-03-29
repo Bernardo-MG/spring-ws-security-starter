6d57ef78339ca6123bd1c26842dc3ee0cdaa24eb
Bernardo <programming@bernardomg.com>
2026-03-29 23:13:47 +0200

    Avoid null list

--- src/main/java/com/bernardomg/security/initializer/configuration/PermissionsFilesProperties.java
+++ src/main/java/com/bernardomg/security/initializer/configuration/PermissionsFilesProperties.java
@@ -2,11 +2,20 @@
 package com.bernardomg.security.initializer.configuration;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.core.io.Resource;
 
 @ConfigurationProperties(prefix = "security.permissions")
 public record PermissionsFilesProperties(Collection<Resource> files) {
+
+    public PermissionsFilesProperties(final Collection<Resource> files) {
+        if (files == null) {
+            this.files = List.of();
+        } else {
+            this.files = files;
+        }
+    }
 
 }
