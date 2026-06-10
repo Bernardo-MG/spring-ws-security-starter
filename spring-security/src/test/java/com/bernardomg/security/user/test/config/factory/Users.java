123aaa3d04607168ae1ca44a3b294e7239cb15f5
Bernardo <programming@bernardomg.com>
2026-06-10 23:24:08 +0200

    Extracted module

---
+++ spring-security/src/test/java/com/bernardomg/security/user/test/config/factory/Users.java
@@ -0,0 +1,35 @@
+
+package com.bernardomg.security.user.test.config.factory;
+
+import java.util.List;
+
+import com.bernardomg.security.user.domain.model.User;
+
+public final class Users {
+
+    public static final User disabled() {
+        return new User(UserConstants.EMAIL, UserConstants.USERNAME, UserConstants.NAME, false, true, true, true,
+            List.of(Roles.withSinglePermission()));
+    }
+
+    public static final User enabled() {
+        return new User(UserConstants.EMAIL, UserConstants.USERNAME, UserConstants.NAME, true, true, true, true,
+            List.of(Roles.withSinglePermission()));
+    }
+
+    public static final User expired() {
+        return new User(UserConstants.EMAIL, UserConstants.USERNAME, UserConstants.NAME, true, false, true, true,
+            List.of(Roles.withSinglePermission()));
+    }
+
+    public static final User locked() {
+        return new User(UserConstants.EMAIL, UserConstants.USERNAME, UserConstants.NAME, true, true, false, true,
+            List.of(Roles.withSinglePermission()));
+    }
+
+    public static final User passwordExpired() {
+        return new User(UserConstants.EMAIL, UserConstants.USERNAME, UserConstants.NAME, true, true, true, false,
+            List.of(Roles.withSinglePermission()));
+    }
+
+}
---
+++ spring-security/src/test/resources/db/queries/security/relationship/role_alternative_permissions_crud.sql
@@ -0,0 +1,5 @@
+INSERT INTO security.role_permissions (role_id, permission_id) VALUES
+   (2, 1),
+   (2, 2),
+   (2, 3),
+   (2, 4);
