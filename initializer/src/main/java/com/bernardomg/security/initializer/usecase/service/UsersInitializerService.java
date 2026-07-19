4595e2d9c503119690ba324a81fee1d99dc5d245
Bernardo <programming@bernardomg.com>
2026-07-19 18:23:26 +0200

    Removed initializer

--- initializer/src/main/java/com/bernardomg/security/initializer/usecase/service/UsersInitializerService.java
+++
@@ -1,8 +0,0 @@
-
-package com.bernardomg.security.initializer.usecase.service;
-
-public interface UsersInitializerService {
-
-    public void initialize();
-
-}
--- initializer/src/test/resources/db/queries/security/relationship/role_alternative_permissions_crud.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO security.role_permissions (role_id, permission_id) VALUES
-   (2, 1),
-   (2, 2),
-   (2, 3),
-   (2, 4);
