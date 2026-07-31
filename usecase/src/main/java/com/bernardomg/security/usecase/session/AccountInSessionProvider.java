d2f238e7228ee8962479a9f7a6a7b8f06d22faab
Bernardo <programming@bernardomg.com>
2026-07-31 23:30:16 +0200

    Use trust resolver

--- usecase/src/main/java/com/bernardomg/security/usecase/session/AccountInSessionProvider.java
+++ usecase/src/main/java/com/bernardomg/security/usecase/session/AccountInSessionProvider.java
@@ -5,8 +5,15 @@ import java.util.Optional;
 
 import com.bernardomg.security.domain.account.model.Account;
 
+/**
+ * Provides the account for the user in session.
+ */
 public interface AccountInSessionProvider {
 
+    /**
+     * Returns the account for the user in session.
+     * @return the account for the user in session
+     */
     public Optional<Account> getCurrentAccount();
 
 }
--- usecase/src/main/java/com/bernardomg/security/usecase/session/UsernameInSessionProvider.java
+++ usecase/src/main/java/com/bernardomg/security/usecase/session/UsernameInSessionProvider.java
@@ -3,8 +3,15 @@ package com.bernardomg.security.usecase.session;
 
 import java.util.Optional;
 
+/**
+ * Provides the username for the user in session.
+ */
 public interface UsernameInSessionProvider {
 
+    /**
+     * Returns the username for the user in session.
+     * @return the username for the user in session
+     */
     public Optional<String> getCurrentUsername();
 
 }
